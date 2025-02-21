cb054d70c10076220725dd5d3654d177d55b025b
csans13 <167492787+csans13@users.noreply.github.com>
2025-02-21 09:53:26 +0700

    feat: add printBoard

--- src/board.java
+++ src/board.java
@@ -84,4 +84,18 @@ public class board {
         }
         return true;
     }
+
+    public void printBoard()
+    // Cetak papan
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
