195cdfeb9f5fe86f4a0e176113776cc93b6cf341
csans13 <167492787+csans13@users.noreply.github.com>
2025-02-22 02:33:26 +0700

    feat: add solve method

--- src/solver.java
+++ src/solver.java
@@ -1,3 +1,50 @@
+import java.util.List;
+
 public class solver {
-    
+    private board board;
+    private List<piece> pieces;
+
+    public solver(board board, List<piece> pieces)
+    {
+        this.board = board;
+        this.pieces = pieces;
+    }
+
+    public boolean solve(int index)
+    {
+        if (index == pieces.size())
+        {
+            return board.isFull(); // Ditemukan solusi apabila semua blok telah dicoba dan papan terisi penuh
+        }
+
+        piece piece = pieces.get(index);
+
+        // Meletakkan blok di setiap posisi dengan rotasi dan pencerminan
+        for (int i = 0; i < board.getRows(); i++)
+        {
+            for (int j = 0; j < board.getCols(); j++)
+            {
+                for (int k = 0; k < 4; k++)
+                // Mencoba 4 bentuk blok (rotasi)
+                {
+                    for (int l = 0; l < 2; l++)
+                    // Mencoba 2 bentuk blok (pencerminan)
+                    {
+                        if (board.isPlaceable(piece, i, j))
+                        {
+                            board.placePiece(piece, i, j, (char) ('A' + index));
+                            if (solve(index + 1))
+                            {
+                                return true;
+                            }
+                            board.removePiece(piece, i, j); // Backtrack
+                        }
+                        piece = piece.flip();
+                    }
+                    piece = piece.rotate();
+                }
+            }
+        }
+        return false;
+    }
 }
