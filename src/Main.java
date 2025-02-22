b46eac93e1707981d963a45f2547dbd97a6fa3c7
csans13 <167492787+csans13@users.noreply.github.com>
2025-02-22 13:08:47 +0700

    feat: complete implementation of main method

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,45 @@ public class Main
 
         System.out.println("Masukkan nama file test case: ");
         String filename = scanner.nextLine();
+        scanner.close();
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("test/" + filename));
+            String[] boardDimAndPieceCount = br.readLine().split(" ");
+            int N = Integer.parseInt(boardDimAndPieceCount[0]);
+            int M = Integer.parseInt(boardDimAndPieceCount[1]);
+            int P = Integer.parseInt(boardDimAndPieceCount[2]);
+
+            Board board = new Board(N, M);
+            List<Piece> pieces = new ArrayList<>();
+
+            for (int i = 0; i < P; i++)
+            {
+                char[][] shape = parsePiece(br);
+                pieces.add(new Piece(shape));
+            }
+            br.close();
+
+            Solver solver = new Solver(board, pieces);
+            long startTime = System.currentTimeMillis();
+            boolean foundSolution = solver.solve(0);
+            long endTime = System.currentTimeMillis();
+
+            if (foundSolution)
+            {
+                board.printBoard();
+                System.out.println("Waktu pencarian: " + (endTime - startTime) + " ms");
+            }
+            else
+            {
+                System.out.println("Puzzle tidak memiliki solusi");
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Gagal membaca file");
+        }
     }
 
     private static char[][] parsePiece(BufferedReader br) throws IOException
