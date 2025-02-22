1df016fb0e78b8cc1b5d7ff455baf945fe42671a
csans13 <167492787+csans13@users.noreply.github.com>
2025-02-22 12:12:03 +0700

    feat: add user input for test case filename

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import java.io.*;
+import java.util.*;
 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Masukkan nama file test case: ");
+        String filename = scanner.nextLine();
+    }
 }
